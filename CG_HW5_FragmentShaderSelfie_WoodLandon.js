cf71056586573545ab41ba880ed3e0f3dec182d9
johnlandonwood <landonw@smu.edu>
2023-11-19 18:40:04 -0600

    Basic fragment shader switch

--- CG_HW5_FragmentShaderSelfie_WoodLandon.js
+++ CG_HW5_FragmentShaderSelfie_WoodLandon.js
@@ -1,160 +1,61 @@
-"use strict";
+const vertices = [
+    -1.0, 1.0, 0.0, 1.0,
+    -1.0, -1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
+    1.0, -1.0, 0.0, 1.0
+];
 
 var canvas;
 var gl;
 
-// quad texture coordinates
-
-var texCoord = [
-    vec2(0, 0),
-    vec2(0, 1),
-    vec2(1, 1),
-    vec2(1, 1),
-    vec2(1, 0),
-    vec2(0, 0)
-];
-
-// quad vertices
-
-var vertices = [
-    vec2(-0.5, -0.5),
-    vec2(-0.5, 0.5),
-    vec2(0.5, 0.5),
-    vec2(0.5, 0.5),
-    vec2(0.5, -0.5),
-    vec2(-0.5, -0.5)
-];
-
-// triangle vertices
-
-var positionsArray = [
-    vec2(-0.5, -0.5),
-    vec2(0, 0.5),
-    vec2(0.5, -0.5)
-];
-// var vertices = [
-//     vec4(0.0,  0.0,  0.0,  1.0),
-//     vec4(1.0,  0.0,  0.0,  1.0),
-//     vec4(1.0,  0.0,  1.0,  1.0),
-//     vec4(0.0,  0.0,  1.0,  1.0)
-// ];
-
-var program1, program2;
-var texture1;
-
-var framebuffer, renderbuffer;
-
+var vBuffer;
 
 window.onload = function init() {
-    canvas = document.getElementById("gl-canvas");
-
+    canvas = document.getElementById('glCanvas');
     gl = canvas.getContext('webgl2');
     if (!gl) alert("WebGL 2.0 isn't available");
-
-
-// Create an empty texture
-
-    texture1 = gl.createTexture();
-    gl.activeTexture( gl.TEXTURE0 );
-    gl.bindTexture( gl.TEXTURE_2D, texture1 );
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 64, 64, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-    gl.generateMipmap(gl.TEXTURE_2D);
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR );
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-
-    gl.bindTexture(gl.TEXTURE_2D, null);
-
-// Allocate a frame buffer object
-
-   framebuffer = gl.createFramebuffer();
-   gl.bindFramebuffer( gl.FRAMEBUFFER, framebuffer);
-
-// Attach color buffer
-
-   gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture1, 0);
-
-// check for completeness
-
-   var status = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
-   if(status != gl.FRAMEBUFFER_COMPLETE) alert('Frame Buffer Not Complete');
-
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-    program1 = initShaders(gl, "vertex-shader1", "fragment-shader1");
-    program2 = initShaders(gl, "vertex-shader2", "fragment-shader2");
-
-    gl.useProgram(program1);
-
-
-    // Create and initialize a buffer object with triangle vertices
-
-    var buffer1 = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer1);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
-
-    // Initialize the vertex position attribute from the vertex shader
-
-    var positionLoc = gl.getAttribLocation(program1, "aPosition");
-    gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray(positionLoc);
-
-    // Render one triangle
-
-    gl.viewport(0, 0, 64, 64);
-    gl.clearColor(0.5, 0.5, 0.5, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-
-
-    // Bind to window system frame buffer, unbind the texture
-
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    gl.disableVertexAttribArray(positionLoc);
-
-    gl.useProgram(program2);
-
-    gl.activeTexture(gl.TEXTURE0);
-
-    gl.bindTexture(gl.TEXTURE_2D, texture1);
-
-    // send data to GPU for normal render
-
-    var buffer2 = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, buffer2);
-    gl.bufferData(gl.ARRAY_BUFFER,   new flatten(vertices), gl.STATIC_DRAW);
-
-    var positionLoc = gl.getAttribLocation( program2, "aPosition");
-    gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionLoc);
-
-
-    var buffer3 = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer3);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoord), gl.STATIC_DRAW);
-
-
-    var texCoordLoc = gl.getAttribLocation( program2, "aTexCoord");
-    gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray(texCoordLoc);
-
-    gl.uniform1i( gl.getUniformLocation(program2, "uTextureMap"), 0);
-
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
-
     gl.viewport(0, 0, 512, 512);
 
-    render();
+    blueProgram = initShaders(gl, "vertex-shader", "fragment-shader1");
+    redProgram = initShaders(gl, "vertex-shader", "fragment-shader2");
+    greenProgram = initShaders(gl, "vertex-shader", "fragment-shader3")
+
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    document.getElementById('shaderSelector').addEventListener('change', function (event) {
+      const selectedIndex = event.target.value;
+      switch (selectedIndex) {
+        case '0':
+          render(blueProgram);
+          break;
+        case '1':
+          render(redProgram);
+          break;
+        case '2':
+          render(greenProgram);
+          break;
+        default:
+          render(blueProgram);
+          break;
+      }
+    });
+
+    render(blueProgram);
 }
 
-
-function render() {
-
-    gl.clearColor(0.0, 0.0, 1.0, 1.0);
+function render(program) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // render quad with texture
+    gl.useProgram(program);
 
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
-}
+
+    const positionAttributeLocation = gl.getAttribLocation(program, 'aPosition');
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.vertexAttribPointer(positionAttributeLocation, 4, gl.FLOAT, false, 0, 0);
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  }
