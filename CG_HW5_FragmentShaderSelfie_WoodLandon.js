b213e375079f405270c60666f66b1004d0aafb62
johnlandonwood <landonw@smu.edu>
2023-11-19 19:17:07 -0600

    about to start img processing

--- CG_HW5_FragmentShaderSelfie_WoodLandon.js
+++ CG_HW5_FragmentShaderSelfie_WoodLandon.js
@@ -1,3 +1,9 @@
+var canvas;
+var gl;
+var vBuffer;
+var blueProgram, redProgram, greenProgram;
+var imgProgram;
+
 const vertices = [
     -1.0, 1.0, 0.0, 1.0,
     -1.0, -1.0, 0.0, 1.0,
@@ -5,10 +11,33 @@ const vertices = [
     1.0, -1.0, 0.0, 1.0
 ];
 
-var canvas;
-var gl;
+var texSize = 256;
+var data = new Uint8Array (3*texSize*texSize);   
+for (var i = 0; i<= texSize; i++)  {
+    for (var j=0; j<=texSize; j++) {
+        data[3*texSize*i+3*j  ] = rawData[i*256+j];
+        data[3*texSize*i+3*j+1] = rawData[i*256+j];
+        data[3*texSize*i+3*j+2] = rawData[i*256+j];
+    }
+}
 
-var vBuffer;
+var texCoords = [
+    vec2(0, 0),
+    vec2(1, 0),
+    vec2(1, 1),
+    vec2(0, 1)
+];
+
+function configureTexture(image, width, height) {
+    console.log(width, height);
+    var texture = gl.createTexture();
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+}
 
 window.onload = function init() {
     canvas = document.getElementById('glCanvas');
@@ -16,13 +45,44 @@ window.onload = function init() {
     if (!gl) alert("WebGL 2.0 isn't available");
     gl.viewport(0, 0, 512, 512);
 
-    blueProgram = initShaders(gl, "vertex-shader", "fragment-shader1");
-    redProgram = initShaders(gl, "vertex-shader", "fragment-shader2");
-    greenProgram = initShaders(gl, "vertex-shader", "fragment-shader3")
+    blueProgram = initShaders(gl, "vertex-shader1", "fragment-shader1");
+    redProgram = initShaders(gl, "vertex-shader1", "fragment-shader2");
+    greenProgram = initShaders(gl, "vertex-shader1", "fragment-shader3")
+    imgProgram = initShaders(gl, "img-vertex-shader", "img-fragment-shader")
 
     vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+
+
+    configureTexture (data, texSize, texSize)
+
+
+    document.getElementById('fileInput').onchange = (e) => {
+        let file = e.target.files[0];
+        let fileReader = new FileReader();
+        fileReader.onload = (e) => {
+            let result = e.target.result;
+            let resultImage = new Image();
+            resultImage.onload = () => {
+                let canvas = document.createElement('canvas');
+                let ctx = canvas.getContext("2d");
+    
+                ctx.drawImage(resultImage, 0, 0, resultImage.width, resultImage.height);
+    
+                let imageData = ctx.getImageData(0, 0, resultImage.width, resultImage.height);
+                console.log(imageData);
+    
+                let image = new Uint8Array(resultImage.width * resultImage.height * 4);
+                for (let i = 0; i < resultImage.width * resultImage.height * 4; i++) image[i] = imageData.data[i];
+    
+                configureTexture(imageData, resultImage.width, resultImage.height);
+            }
+            resultImage.src = result;
+        }
+        fileReader.readAsDataURL(file);
+    }
 
     document.getElementById('shaderSelector').addEventListener('change', function (event) {
       const selectedIndex = event.target.value;
@@ -36,11 +96,16 @@ window.onload = function init() {
         case '2':
           render(greenProgram);
           break;
+        case '3':
+          render(imgProgram);
+          break;
         default:
-          render(blueProgram);
+          render(imgProgram);
           break;
       }
     });
+
+
 
     render(blueProgram);
 }
@@ -56,6 +121,14 @@ function render(program) {
     gl.enableVertexAttribArray(positionAttributeLocation);
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.vertexAttribPointer(positionAttributeLocation, 4, gl.FLOAT, false, 0, 0);
+
+    var tBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoords), gl.STATIC_DRAW);
+
+    var texCoordLoc = gl.getAttribLocation( program, "aTexCoord");
+    gl.vertexAttribPointer( texCoordLoc, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(texCoordLoc);
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
   }
